078837bded41307a330a15891cce356b77015a43
nopall <nouvalfarras15@gmail.com>
2020-12-03 06:07:24 +0700

    js done

--- asyncawait.js
+++ asyncawait.js
@@ -1,14 +1,5 @@
-/* styling */
-require("styles/main.scss");
-/* js */
-import $ from "jquery";
-import { log, logTitle } from "logger";
-/* your imports */
-logTitle("Async Await");
-/* coding examples */
-
 const logName = async (name) => {
-  log(name);
+  console.log(name);
   // 1. we can yield promises using await
   const transformName = new Promise((resolve, reject) => {
     setTimeout(() => resolve(name.toUpperCase()), 1000);
@@ -21,7 +12,7 @@ const logName = async (name) => {
 }
 
 logName("antonio").then(res => {
-  log("result from async function = " + res);
+  console.log("result from async function = " + res);
 });
 
 ////////////
